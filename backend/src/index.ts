2a5a5d365967428b9219eb8e9cceb45158e541fe
antonfoch <antonfoch@orange.fr>
2024-07-25 22:41:48 +0200

    Setup frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 
 import { createClient } from "@supabase/supabase-js";
 import * as dotenv from "dotenv";
@@ -10,17 +11,19 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors())
+
 
 const supabaseUrl = process.env.SUPABASE_URL as string;
 const supabaseKey = process.env.SUPABASE_ANON_KEY as string;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.get("/Test", async (req: Request, res: Response) => {
-  const { data: test, error } = await supabase.from("test").select("test");
+  const { data: test2, error } = await supabase.from("test2").select();
   if (error) {
     return res.status(400).json(error);
   }
-  return res.status(200).json(test);
+  return res.status(200).json(test2);
 });
 
 app.listen(port, () => {
