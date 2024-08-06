4a95704d7d173ce0f767a4444499b407c051378d
antonfoch <antonfoch@orange.fr>
2024-08-06 23:47:53 +0200

    Fixed: event is added to localstorage

--- frontend/src/components/Calendar/Calendar.svelte
+++ frontend/src/components/Calendar/Calendar.svelte
@@ -3,10 +3,9 @@
     today,
     getLocalTimeZone,
     getDayOfWeek,
-    CalendarDate,
   } from "@internationalized/date";
   import { Button } from "$lib/components/ui/button";
-  import { get } from "svelte/store";
+  import { onMount, afterUpdate } from "svelte";
 
   const semaine = [
     "Lundi",
@@ -33,46 +32,56 @@
     "Décembre",
   ];
 
-  let list: any = [];
+  let list: any[] = [];
   let date = today(getLocalTimeZone());
   let dayOfWeek = getDayOfWeek(date, "fr-FR");
-
-  let startDate = date.subtract({ days: dayOfWeek });
-  for (let i = 0; i < 7; i++) {
-    let dateInc = startDate.add({ days: i });
-    list.push(
-      semaine[i] +
-        " " +
-        dateInc.day.toString() +
-        " " +
-        months[dateInc.month - 1]
-    );
-  }
+  let Event = "";
 
   function updateList() {
-    list = [];
+    dayOfWeek = getDayOfWeek(date, "fr-FR");
+    let startDate = date.subtract({ days: dayOfWeek });
 
     for (let i = 0; i < 7; i++) {
+      if (localStorage.getItem(startDate.add({ days: i }).toString())) {
+        Event =
+          localStorage.getItem(startDate.add({ days: i }).toString()) ?? "";
+      } else {
+        Event = "";
+      }
       let dateInc = startDate.add({ days: i });
-      list.push(
-        semaine[i] +
-          " " +
-          dateInc.day.toString() +
-          " " +
-          months[dateInc.month - 1]
-      );
+
+      let obj = {
+        week: semaine[i],
+        day: dateInc.day.toString(),
+        month: months[dateInc.month - 1],
+        event: Event,
+        id: dateInc.toString(),
+      };
+      list[i] = obj;
     }
+  }
+
+  function addEvent(id: string) {
+    let index = list.findIndex((item) => item.id === id);
+
+    const added = (list[index].event =
+      `nous sommes le ${list[index].week} ${list[index].day} ${list[index].month}`);
+    localStorage.setItem(id, added);
   }
 
   function nextWeek() {
     date = date.add({ days: 7 });
+
     updateList();
   }
 
   function previousWeek() {
     date = date.subtract({ days: 7 });
+
     updateList();
   }
+
+  updateList();
 </script>
 
 <div class="flex flex-col gap-5 p-5 h-screen">
@@ -83,14 +92,21 @@
     </h1>
     <Button on:click={nextWeek}>Next</Button>
   </div>
+
   <div class="flex gap-3 justify-between">
     {#each list as day}
       <div class="w-full bg-card border border-border rounded-md px-5 py-3">
-        <p class=" text-lg lg:text-xl">{day.split(" ")[0]}</p>
+        <p class=" text-lg lg:text-xl">{day.week}</p>
 
         <p class=" text-muted-foreground text-xs">
-          {" " + day.split(" ").slice(1).join(" ")}
+          {day.day}
+          {day.month}
         </p>
+
+        <p class="text-sm">{day.event}</p>
+        <div class="flex justify-end">
+          <Button on:click={() => addEvent(day.id)}>Add Event</Button>
+        </div>
       </div>
     {/each}
   </div>
---
+++ frontend/src/store/store.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import { writable } from "svelte/store";
+
+  export const list = writable([]);
+</script>
